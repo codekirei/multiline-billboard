3b42f7df8b5615774a0059621cf78c71ed3b12f9
Jacob Blakely <jacob@codekirei.com>
2016-01-22 15:13:13 -0800

    write basic tests

--- test/index.js
+++ test/index.js
@@ -3,9 +3,49 @@
 //----------------------------------------------------------
 // modules
 //----------------------------------------------------------
+// node
+import {EOL} from 'os'
+
+// npm
+import test from 'ava'
+import ansi from 'ansi-styles'
+
 // local
-const billboard = require('../')
+import billboard from '../'
 
 //----------------------------------------------------------
 // tests
 //----------------------------------------------------------
+const bo = ansi.blue.open
+const bc = ansi.blue.close
+const to = ansi.white.open
+const tc = ansi.white.close
+
+test('strs', t => {
+  const strs =
+    [ 'Standing in hallway'
+    , 'For forgetting my homework'
+    , 'Mogami River']
+  const out = billboard(strs)
+  const fill = '═'.repeat(strs[1].length)
+  const expected =
+    [ `${bo}╔═${fill}═╗`
+    , `║${bc} ${to}Standing in hallway       ${tc} ${bo}║`
+    , `║${bc} ${to}For forgetting my homework${tc} ${bo}║`
+    , `║${bc} ${to}Mogami River              ${tc} ${bo}║`
+    , `╚═${fill}═╝${bc}`
+    ]
+  out.split(EOL).map((str, i) => t.is(str, expected[i]))
+})
+
+test('str', t => {
+  const str = 'This is not a test.'
+  const out = billboard(str)
+  const fill = '═'.repeat(str.length)
+  const expected =
+    [ `${bo}╔═${fill}═╗`
+    , `║${bc} ${to}${str}${tc} ${bo}║`
+    , `╚═${fill}═╝${bc}`
+    ]
+  out.split(EOL).map((str, i) => t.is(str, expected[i]))
+})
