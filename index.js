0c952def5bae480e43ae1d024501b6fc2871c630
Jacob Blakely <jacob@codekirei.com>
2016-01-22 14:44:17 -0800

    modularize logic

--- index.js
+++ index.js
@@ -3,17 +3,15 @@
 //----------------------------------------------------------
 // modules
 //----------------------------------------------------------
-// node
-const EOL = require('os').EOL
-
 // npm
-const ansi = require('ansi-styles')
 const merge = require('lodash.merge')
+
+// local
+const lib = require('./lib')
 
 //----------------------------------------------------------
 // logic
 //----------------------------------------------------------
-const SPACE = ' '
 const defaultOpts =
   { colors:
     { text: 'white'
@@ -29,89 +27,18 @@ const defaultOpts =
     }
   }
 
-/**
-  Find character count of longest string in array.
-
-  @param {String[]} ar - array of strings
-  @returns {Number} number of characters in longest string
-  */
-const longest = ar =>
-  ar.reduce((prev, cur) => {
-    const len = cur.length
-    return len > prev ? len : prev
-  }, 0)
-
-// doc me
-const top = (fill, opts) =>
-  [ ansi[opts.colors.border].open
-  , opts.border.ul
-  , opts.border.horizontal
-  , fill
-  , opts.border.horizontal
-  , opts.border.ur
-  , EOL
-  ].join('')
-
-// doc me
-const bot = (fill, opts) =>
-  [ opts.border.ll
-  , opts.border.horizontal
-  , fill
-  , opts.border.horizontal
-  , opts.border.lr
-  , ansi[opts.colors.border].close
-  ].join('')
-
-/**
-  Pad a string with spaces to a specified length.
-
-  @param {Number} max - length to pad to
-  @returns {Function} curried fn
- */
-const pad = max => str => {
-  const len = str.length
-  return max > len ? str + ' '.repeat(max - len) : str
-}
-
-// doc me
-const wrap = opts => str =>
-  [ opts.border.vertical
-  , ansi[opts.colors.border].close
-  , SPACE
-  , ansi[opts.colors.text].open
-  , str
-  , ansi[opts.colors.text].close
-  , SPACE
-  , ansi[opts.colors.border].open
-  , opts.border.vertical
-  , EOL
-  ].join('')
-
-// doc me
-function handleStrs(strs, opts) {
-  const len = longest(strs)
-  const text = strs
-    .map(pad(len))
-    .map(wrap(opts))
-    .join('')
-  return {len, text}
-}
-
-// doc me
-function handleStr(str, opts) {
-  const len = str.length
-  const text = wrap(opts)(str)
-  return {len, text}
-}
-
 // doc me
 function billboard(strOrStrs, customOpts) {
   const opts = merge({}, defaultOpts, customOpts)
   const mid = typeof strOrStrs === 'object'
-    ? handleStrs(strOrStrs, opts)
-    : handleStr(strOrStrs, opts)
+    ? lib.handleStrs(strOrStrs, opts)
+    : lib.handleStr(strOrStrs, opts)
   const fill = opts.border.horizontal.repeat(mid.len)
-  return [top(fill, opts), mid.text, bot(fill, opts)].join('')
+  return [
+    lib.top(fill, opts)
+    , mid.text
+    , lib.bot(fill, opts)
+  ].join('')
 }
 
 //----------------------------------------------------------
