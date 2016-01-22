bf30999886272b7dc0c8cd7347bee3677a13457d
Jacob Blakely <jacob@codekirei.com>
2016-01-21 18:00:45 -0800

    add logic to find longest str

--- index.js
+++ index.js
@@ -10,23 +10,38 @@ const merge = require('lodash.merge')
 //----------------------------------------------------------
 // logic
 //----------------------------------------------------------
-function billboard(strOrStrs, customOpts) {
-  // handle opts
-  const defaultOpts =
-    { colors:
-      { text: 'blue'
-      , border: 'white'
-      }
-    , border:
-      { ul: '╔'
-      , lr: '╝'
-      , ur: '╗'
-      , ll: '╚'
-      , vertical: '║'
-      , horizontal: '═'
-      }
+const defaultOpts =
+  { colors:
+    { text: 'blue'
+    , border: 'white'
     }
+  , border:
+    { ul: '╔'
+    , lr: '╝'
+    , ur: '╗'
+    , ll: '╚'
+    , vertical: '║'
+    , horizontal: '═'
+    }
+  }
+
+/**
+  Find character count of longest string in array.
+
+  @param {String[]} ar - array of strings
+  @returns {Number} number of characters in longest string
+  */
+const longest = ar =>
+  ar.reduce((prev, cur) => {
+    const len = cur.length
+    return len > prev ? len : prev
+  }, 0)
+
+function billboard(strOrStrs, customOpts) {
   const opts = merge({}, defaultOpts, customOpts)
+  const len = typeof strOrStrs === 'object'
+    ? longest(strOrStrs)
+    : strOrStrs.length
 }
 
 //----------------------------------------------------------
